fa2b9a45d4e27d9c6a8ed14f74eb8df62e43d7ed
Nguyễn Việt <vietbmt19@gmail.com>
2025-01-16 03:11:45 +0700

    npm nodejs

---
+++ test-module/NPM/index.js
@@ -0,0 +1,8 @@
+const sum = (a, b) => a + b;
+
+console.log(sum(2, 5))
+
+//ban cap nhap mmp  Major.Minor.Patch
+//^ cai duoc minor voi pathch
+//~ chi cai duoc patch thoi
+//~ minor nhung chuc nang moi duoc thay vao
